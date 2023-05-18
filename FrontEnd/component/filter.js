c9e9c36e23cb4e3692a81a92927c8c0461e9a6f8
Moulay-E <Moulay553E@gmail.com>
2023-05-18 09:56:04 +0200

    adjust login.html and index

---
+++ FrontEnd/component/filter.js
@@ -0,0 +1,5 @@
+
+const all = document.querySelectorAll(".portfolio__btn__all");
+const object = document.querySelectorAll(".portfolio__btn__object");
+const apartment = document.querySelectorAll(".portfolio__btn__apartment");
+const hotel = document.querySelectorAll(".portfolio__btn__hotel");
