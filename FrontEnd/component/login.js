9ef723c2a98b0772cba97bb99cda81f21bdea424
Moulay-E <Moulay553E@gmail.com>
2023-05-21 10:59:35 +0200

    add function check status, autorisationLogin

--- FrontEnd/component/login.js
+++ FrontEnd/component/login.js
@@ -1,4 +1,5 @@
 export let tokens;
+export let status;
 const autentification = {
   email: "sophie.bluel@test.tld",
   password: "S0phie",
@@ -14,29 +15,52 @@ LoginForm.addEventListener("submit", function (event) {
   };
   console.dir(tryLogin);
   const loginJson = JSON.stringify(tryLogin);
-  fectchLogin(loginJson).then(() => {
-    const pass = document.querySelector("[name=password]");
-    pass.style.backgroundColor = "red";
+  fetchLogin(loginJson).then(()=>{
+    AutorisationLogin(status)
   });
 });
 
-function fectchLogin(loginJson) {
-  fetch("http://localhost:5678/api/users/login", {
+function fetchLogin(loginJson) {
+  return fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: loginJson,
   })
-    .then((response) => response.json())
+    .then((response) => {
+      status = response.status; 
+      console.log(status);
+
+    return response.json();
+    })
     .then((data) => {
+      console.dir(data);
       // Traitez les données reçues du serveur ici
       tokens = data;
       console.log(data);
       console.log(tokens.token + " voici le token ");
+      return tokens;
     })
     .catch((error) => {
       // Gérez les erreurs ici
       console.error(error);
     });
-}
+};
+
+
+function AutorisationLogin(status) {
+  const error = document.querySelector(".error");
+  const pass = document.querySelector("[name=password]");
+  if (status === 200){
+    console.log("vous êtes connecter");
+    error.style.display = "none";
+    pass.style.backgroundColor = "white";
+  }
+  else{
+    console.log("vous ete pas co");
+     pass.style.backgroundColor = "red";
+     error.style.display = "block";
+  }
+
+};
